39bff3c27905f551880696fefe2e780f85212c24
junSung <not971006@gmail.com>
2025-07-29 20:47:25 +0900

    백준 5639 이진탐색

--- 5639.py
+++ 5639.py
@@ -1,13 +1,26 @@
 import sys
+sys.setrecursionlimit(20000)
 
-input = sys.stdin.readline
+preorder = []
+while True:
+    try:
+        preorder.append(int(input()))
+    except EOFError:
+        break
 
-preorder = list(map(int, input().split()))
-print(preorder)
+def postorder(start, end):
+    if start > end:
+        return
+    
+    root = preorder[start]
+    idx = start + 1
+    
+    while idx <= end and preorder[idx] < root:
+        idx += 1
+    
+    postorder(start + 1, idx - 1)
+    postorder(idx, end)
+    print(root)
 
-# right = []
-
-# for p in preorder:
-#     if preorder[0] < p:
-#         right.append(p)
-
+if preorder:
+    postorder(0, len(preorder) - 1)
